e8d7e198d8b496b4386b5a48cf68a158c5aa5805
Byeonghyun Pak <byeonghyun.pak@gmail.com>
2024-12-19 09:13:32 +0900

    add make_coord function

--- geometry.py
+++ geometry.py
@@ -1,6 +1,21 @@
 import torch
 import numpy as np
 
+def make_coord(shape, ranges=None, flatten=True):
+    coord_seqs = []
+    for i, n in enumerate(shape):
+        if ranges is None:
+            v0, v1 = -1, 1
+        else:
+            v0, v1 = ranges[i]
+        r = (v1 - v0) / (2 * n)
+        seq = v0 + r + (2 * r) * torch.arange(n).float()
+        coord_seqs.append(seq)
+    ret = torch.stack(torch.meshgrid(*coord_seqs), dim=-1)
+    if flatten:
+        ret = ret.view(-1, ret.shape[-1])
+    return ret
+
 def rodrigues_torch(rvec):
     theta = torch.norm(rvec)
     if theta < torch.finfo(torch.float32).eps:
