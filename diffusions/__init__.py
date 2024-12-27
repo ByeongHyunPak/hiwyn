6fddfa2946fe1df4f958ff65b1dbd6d24e2ca4c9
Junwha Hong <qbit@unist.ac.kr>
2024-12-27 15:58:52 +0000

    Weaken mask proportional to phi level

--- diffusions/__init__.py
+++ diffusions/__init__.py
@@ -3,6 +3,6 @@ from .ERPdiffusion_0_0_0 import ERPDiffusion_0_0_0
 from .ERPdiffusion_0_1_0 import ERPDiffusion_0_1_0
 from .ERPdiffusion_0_1_1 import ERPDiffusion_0_1_1
 from .ERPdiffusion_0_1_2 import ERPDiffusion_0_1_2
-
+from .ERPdiffusion_0_1_3 import ERPDiffusion_0_1_3
 from .ERPdiffusion_0_1_4 import ERPDiffusion_0_1_4
 from .ERPdiffusion_0_1_5 import ERPDiffusion_0_1_5
