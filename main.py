2c30ead43a580f0eb020cee8171684b1d5e0ad55
Byeonghyun Pak <byeonghyun.pak@gmail.com>
2024-12-27 15:46:56 +0900

    bug fix: no fov in args

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ if __name__ == '__main__':
     parser.add_argument('--num_theta', nargs="+", type=int, default=[3, 4, 6, 4, 3])
     parser.add_argument('--phi_range', nargs=2, type=int, default=[-45, 45])
     parser.add_argument('--num_phi', type=int, default=5)
+    parser.add_argument('--fov', type=int, default=90)
     
     args = parser.parse_args()
     
@@ -57,27 +58,7 @@ if __name__ == '__main__':
         print(*directions[-args.num_theta[i]:])   
     
     # get ERPDiffusion model
+    H, W = args.erp_hw
     ERPDiffusion = globals()[f"ERPDiffusion_{args.erpdm_version.replace('.', '_')}"]   
-
-    try:
-        H, W = args.erp_hw
-        sd = ERPDiffusion(device=torch.device('cuda'), sd_version=args.sd_version, fov=args.fov, views=directions) 
-        outputs = sd.text2erp(
-            args.prompt, args.negative, height=H, width=W, num_inference_steps=args.steps, save_dir=save_dir)
-        
-        del outputs
-        del sd
-        del ERPDiffusion
-        torch.cuda.empty_cache()
-        gc.collect()
-        torch.cuda.empty_cache()
-        
-    except Exception:
-        
-        del sd
-        del ERPDiffusion
-        torch.cuda.empty_cache()
-        gc.collect()
-        torch.cuda.empty_cache()
-       
-        print(traceback.format_exc())
+    sd = ERPDiffusion(device=torch.device('cuda'), sd_version=args.sd_version, fov=args.fov, views=directions) 
+    outputs = sd.text2erp(args.prompt, args.negative, height=H, width=W, num_inference_steps=args.steps, save_dir=save_dir)
