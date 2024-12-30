54ab5aabe7c195217a330a00894fdc6378af9da2
Byeonghyun Pak <byeonghyun.pak@gmail.com>
2024-12-30 20:57:09 +0900

    minor typo fix

--- diffusions/__init__.py
+++ diffusions/__init__.py
@@ -9,4 +9,4 @@ from .ERPdiffusion_0_1_5 import ERPDiffusion_0_1_5
 from .ERPdiffusion_0_1_6 import ERPDiffusion_0_1_6
 
 from .ERPdiffusion_0_2_0 import ERPDiffusion_0_2_0
-from .ERPdiffusion_0_2_0 import ERPDiffusion_0_2_1
+from .ERPdiffusion_0_2_1 import ERPDiffusion_0_2_1
