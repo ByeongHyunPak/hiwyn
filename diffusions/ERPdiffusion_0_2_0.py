bde0b1c84c846ea54faf8690e6989cecceb5896c
Byeonghyun Pak <byeonghyun.pak@gmail.com>
2024-12-30 17:42:47 +0900

    New ERPDiffusion_0_2_0 - Deepfloyd+MultiDiff. ERP

---
+++ diffusions/ERPdiffusion_0_2_0.py
@@ -0,0 +1,131 @@
+import os
+import torch
+import torch.nn.functional as F
+
+from tqdm import tqdm
+from torchvision.transforms import ToPILImage
+
+from diffusions import ERPDiffusion_0_1_1
+from geometry import make_coord, gridy2x_erp2pers
+
+
+class ERPDiffusion_0_2_0(ERPDiffusion_0_1_1):
+    """ MultiDiffusion w/ DeepFloyd-IF
+    """
+    
+    def __init__(self, 
+                 device, 
+                 hf_key=None, 
+                 fov=90,
+                 views=[(0, 0), (45, 0)],
+                 half_precision=False
+                 ):
+        super().__init__(device, hf_key, half_precision)
+
+        self.up_level = 3
+        
+        self.views = views
+        self.fov = fov
+
+        self.fin_v_nums = []
+        self.indices = None
+    
+    @torch.no_grad()
+    def text2erp(self,
+                 prompts, 
+                 negative_prompts='', 
+                 height=512, width=1024, 
+                 num_inference_steps=50,
+                 guidance_scale=7.5,
+                 save_dir=None):
+        
+        if isinstance(prompts, str):
+            prompts = [prompts]
+        
+        if isinstance(negative_prompts, str):
+            negative_prompts = [negative_prompts]
+
+        # Prompts -> text embeds
+        text_embeds = self.get_text_embeds(prompts, negative_prompts)  # [2, 77, 768]
+
+        # 1) Initialize x^T_ERP
+        x_erp = torch.randn((1, self.unet.in_channels, height // self.resolution_factor, width // self.resolution_factor), device=self.device)
+
+        # 2) Conditional Upsampling x^T_ERP
+        x_erp_up = self.cond_noise_sampling(x_erp)
+
+        # 3) Discrete warping x^T_ERP to {w^T_i}_i=1:N
+        w_js = self.discrete_warping(x_erp_up)
+        buffer_imgs = self.decode_and_save(-1, w_js, save_dir, [])
+
+        # set scheduler
+        value = torch.zeros_like(x_erp_up)
+        count = torch.zeros_like(x_erp_up)
+        self.scheduler.set_timesteps(num_inference_steps)
+
+        for i, t in enumerate(tqdm(self.scheduler.timesteps)):
+            if os.path.exists(f"{save_dir}/{i+1:0>2}") is False:
+                    os.mkdir(f"{save_dir}/{i+1:0>2}/")
+
+            value.zero_()
+            count.zero_()
+
+            for j, w_j in enumerate(w_js):
+
+                # 4) Predict e_theta(w^t_j, t)
+                unet_input = torch.cat([w_j] * 2)
+                noise_pred = self.unet(unet_input, t, encoder_hidden_states=text_embeds)['sample']
+
+                # perform guidance
+                noise_pred_uncond, noise_pred_cond = noise_pred.chunk(2)
+                noise_pred_uncond, _ = noise_pred_uncond.split(unet_input.shape[1], dim=1)
+                noise_pred_cond, predicted_variance = noise_pred_cond.split(unet_input.shape[1], dim=1)
+                noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_cond - noise_pred_uncond)
+                noise_pred = torch.cat([noise_pred, predicted_variance], dim=1)
+                noise_pred, _ = noise_pred.split(unet_input.shape[1], dim=1)
+
+                # 5) Get w'^t-1_j
+                w_j_denoised = self.scheduler.step(noise_pred, t, w_j)['prev_sample']
+
+                # 6) inverse mapping w -> x
+                x_erp_up_j, mask_x_j = self.img_j_to_erp(w_j_denoised, j)
+
+                value[:, :] += x_erp_up_j * mask_x_j
+                count[:, :] += mask_x_j
+            
+            # save count map
+            count_ = count / count.max()
+            count_ = ToPILImage()(count_.cpu()[0][0])
+            count_.save(f'/{save_dir}/{i+1:0>2}/count.png')
+
+            # 7) Aggregate on canonical space
+            x_erp_up = value / (count + 1e-8)
+
+            # 8) forward mapping x -> w
+            w_js = self.erp_to_img_j(x_erp_up)
+            buffer_imgs = self.decode_and_save(i, w_js, save_dir, buffer_imgs)
+        
+        return buffer_imgs
+
+    def forward_mapping(self, x):
+        tgts = self.discrete_warping(x, normalize_fin_v_val=False)
+        return tgts
+
+    def inverse_mapping(self, w_j, j):
+        
+        B, C, h, w = w_j.shape
+        H, W  = self.indices[j].shape
+
+        sub_x_j = torch.zeros(B*C, H*W, device=self.device)
+        w_j_flat = w_j.reshape(B*C, -1) / self.fin_v_nums[j].view(1, -1)
+        w_j_flat_pad = torch.zeros(B*C, h*w + 1, device=self.device)
+        w_j_flat_pad[:, 1:] = w_j_flat
+
+        ind_x2w_j = self.indices[j]
+        mask_x_j = ind_x2w_j > 0
+        
+        sub_x_j[:, ...] = w_j_flat_pad[:, ind_x2w_j.flatten()]
+        sub_x_j = sub_x_j.reshape(B, C, H, W)
+        
+        return sub_x_j, mask_x_j
+        
