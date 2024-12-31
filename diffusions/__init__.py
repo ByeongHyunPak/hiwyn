2828dd175554899e713d58467403bf0b29ad1080
Byeonghyun Pak <byeonghyun.pak@gmail.com>
2024-12-31 13:52:12 +0900

    Add statement of each model

--- diffusions/__init__.py
+++ diffusions/__init__.py
@@ -1,14 +1,15 @@
 from .multidiffusion import MultiDiffusion, seed_everything
-from .multidiffusion_2 import MultiDiffusion_2, seed_everything
+from .multidiffusion_2 import MultiDiffusion_2
+from .multidiffusion_2 import MultiDiffusion_3
 
 from .ERPdiffusion_0_0_0 import ERPDiffusion_0_0_0
 from .ERPdiffusion_0_1_0 import ERPDiffusion_0_1_0
 from .ERPdiffusion_0_1_1 import ERPDiffusion_0_1_1 # best!
-from .ERPdiffusion_0_1_2 import ERPDiffusion_0_1_2
-from .ERPdiffusion_0_1_3 import ERPDiffusion_0_1_3
-from .ERPdiffusion_0_1_4 import ERPDiffusion_0_1_4
-from .ERPdiffusion_0_1_5 import ERPDiffusion_0_1_5
-from .ERPdiffusion_0_1_6 import ERPDiffusion_0_1_6
+from .ERPdiffusion_0_1_2 import ERPDiffusion_0_1_2 # something wrong
+from .ERPdiffusion_0_1_3 import ERPDiffusion_0_1_3 # not effective
+from .ERPdiffusion_0_1_4 import ERPDiffusion_0_1_4 # not effective
+from .ERPdiffusion_0_1_5 import ERPDiffusion_0_1_5 # not working
+from .ERPdiffusion_0_1_6 import ERPDiffusion_0_1_6 # deprecated
 
 from .ERPdiffusion_0_2_0 import ERPDiffusion_0_2_0
-from .ERPdiffusion_0_2_1 import ERPDiffusion_0_2_1
+from .ERPdiffusion_0_2_1 import ERPDiffusion_0_2_1 # something wrong
